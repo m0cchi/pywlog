b1866d9adaf2611196c40bd6241fbf2e6d3a2578
m0cchi <boom.boom.planet@gmail.com>
2017-01-22 23:35:55 +0900

    impl model to str

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 
 class Tag(models.Model):
     name = models.CharField(max_length = 25)
+    def __str__(self):
+        return self.name
 
 class Article(models.Model):
     title = models.CharField(max_length = 25)
@@ -11,9 +13,13 @@ class Article(models.Model):
     create_user = models.ForeignKey(User, related_name='article_create_user')
     update_date = models.DateTimeField(auto_now = True)
     update_user = models.ForeignKey(User, related_name='article_update_user')
+    def __str__(self):
+        return '{} {}'.format(self.title, self.create_date)
 
 class TagMapper(models.Model):
     tag = models.ForeignKey(Tag)
     article = models.ForeignKey(Article)
+    def __str__(self):
+        return '{} {}'.format(self.article.title, self.tag.name)
     class Meta:
         unique_together = (('tag', 'article'))
