22278d582eaba49004eb9c4c464613b220e9ffc4
m0cchi <boom.boom.planet@gmail.com>
2017-01-29 17:37:21 +0900

    add login page

--- pywlog/settings.py
+++ pywlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6_8&n66@f9%8mqkxcsk#pvh=!7wf5hdl%kf&meg9dtp3v)_jrn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+    
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
