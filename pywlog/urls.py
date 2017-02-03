807024b1aa73eda9838903c21fc66724abcacd4d
m0cchi <boom.boom.planet@gmail.com>
2017-02-03 18:30:52 +0900

    redirect /blog

--- pywlog/urls.py
+++ pywlog/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth.views import login
+from django.shortcuts import redirect
+
+def redirect_blog(request):
+    return redirect('blog/')
 
 urlpatterns = [
+    url(r'^$', redirect_blog),
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls')),
     url(r'^accounts/login/$', login),
